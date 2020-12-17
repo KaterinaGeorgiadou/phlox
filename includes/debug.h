1fd30ccf4bf11ee201573b1a851475664ccd6625
KaterinaGeorgiadou <GeorgiadouKaterina@outlook.com>
2020-12-17 16:39:31 +0200

    Disassembling Chuncks
    Add functions to test Chuncks' validity

---
+++ includes/debug.h
@@ -0,0 +1,9 @@
+#ifndef phlox_debug_h
+#define phlox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+int disassembleInstruction(Chunk *chunk, int offset);
+
+#endif
