d906d58a8d326c385f1720a58ca54f74912c2794
KaterinaGeorgiadou <GeorgiadouKaterina@outlook.com>
2021-01-07 17:08:38 +0200

    An Arithmetic Calculator
    Support OpCodes for addition, subtraction, multiplication and division.

--- src/vm.c
+++ src/vm.c
@@ -36,6 +36,12 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+	do {\
+		double b = pop();\
+		double a = pop();\
+		push (a op b);\
+	} while (false)
 
 	for (; ;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -51,6 +57,21 @@ static InterpretResult run()
 		uint8_t instruction = READ_BYTE();
 
 		switch (instruction) {
+			case OP_ADD:
+				BINARY_OP(+); 
+				break;
+			case OP_SUBTRACT:
+				BINARY_OP(-);
+				break;
+			case OP_MULTIPLY:
+				BINARY_OP(*);
+				break;
+			case OP_DIVIDE:
+				BINARY_OP(/);
+				break;
+			case OP_NEGATE:
+				push(-pop());
+				break;
 			case OP_RETURN: 
 				printValue(pop());
 				printf("\n");
@@ -66,6 +87,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
